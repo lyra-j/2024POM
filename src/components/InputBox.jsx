18b256a6db20c713ba9d22695db77d54486ece3d
lyra <mk.lyra.j@gmail.com>
2025-01-24 05:48:31 +0900

    fix:placeholder 추가

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export const InputBox = ({ label, id, value, onChange }) => {
+export const InputBox = ({ label, id, value, placeholder, onChange }) => {
   return (
     <label>
       {label}
-      <input type="text" id={id} value={value} onChange={onChange} />
+      <input type="text" id={id} value={value} placeholder={placeholder} onChange={onChange} />
     </label>
   );
 };
