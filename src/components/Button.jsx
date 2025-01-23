3dc3a757f397f77444de36d1682bf8649556d543
lyra <mk.lyra.j@gmail.com>
2025-01-24 08:52:49 +0900

    fix:className 추가

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export const Button = ({ children, onClick, type }) => {
+export const Button = ({ children, onClick, type, className }) => {
   return (
     <>
-      <button type={type} onClick={onClick}>
+      <button type={type} className={className} onClick={onClick}>
         {children}
       </button>
     </>
